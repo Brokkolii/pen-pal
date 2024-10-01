59f24f1eef4e3232e2a7e957cb7028476cf7a686
Patrick Girnghuber <patrick.girnghuber@icloud.com>
2024-10-02 00:27:34 +0200

    ip test

--- src/main/java/com/girnghuber/penpal/resource/LetterResource.java
+++ src/main/java/com/girnghuber/penpal/resource/LetterResource.java
@@ -6,6 +6,7 @@ import com.girnghuber.penpal.entity.Letter;
 import com.girnghuber.penpal.mapper.LetterMapper;
 import com.girnghuber.penpal.service.LetterService;
 import io.quarkus.security.Authenticated;
+import io.vertx.ext.web.RoutingContext;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
@@ -23,13 +24,22 @@ public class LetterResource {
     LetterService letterService;
     @Inject
     LetterMapper letterMapper;
-    @Context
-    SecurityContext securityContext;
+
+    @Inject
+    RoutingContext context;
+
+    @GET
+    @Path("/ip")
+    public String getIp() {
+        String ip = context.request().host();
+        return ip;
+    }
+
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/inbox")
-    public List<LetterDto> getLetters() {
+    public List<LetterDto> getLetters(@Context SecurityContext securityContext) {
         String userId = securityContext.getUserPrincipal().getName();
         List<Letter> letters = letterService.getLettersForUser(userId);
         return letters.stream().map(letterMapper::letterToLetterDto).toList();
@@ -40,7 +50,7 @@ public class LetterResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/send")
-    public LetterDto sendLetter(SendLetterDto sendLetterDto) {
+    public LetterDto sendLetter(SendLetterDto sendLetterDto, @Context SecurityContext securityContext) {
         Letter letter = letterMapper.sendLetterDtoToLetter(sendLetterDto);
         letterService.sendLetter(letter, securityContext.getUserPrincipal().getName());
         return letterMapper.letterToLetterDto(letter);
@@ -48,19 +58,19 @@ public class LetterResource {
 
     @PUT
     @Path("/{letterId}/read")
-    public void markLetterAsRead(@PathParam("letterId") Long letterId) {
+    public void markLetterAsRead(@PathParam("letterId") Long letterId, @Context SecurityContext securityContext) {
         letterService.markLetterAsRead(letterId, securityContext.getUserPrincipal().getName());
     }
 
     @PUT
     @Path("/{letterId}/unread")
-    public void markLetterAsUnread(@PathParam("letterId") Long letterId) {
+    public void markLetterAsUnread(@PathParam("letterId") Long letterId, @Context SecurityContext securityContext) {
         letterService.markLetterAsUnread(letterId, securityContext.getUserPrincipal().getName());
     }
 
     @GET
     @Path("inDelivery")
-    public Boolean letterIsOnTheWay() {
+    public Boolean letterIsOnTheWay(@Context SecurityContext securityContext) {
         return letterService.letterIsOnTheWay(securityContext.getUserPrincipal().getName());
     }
 }
